5b71056f4b025a0551d1fc5f0962fb4be961cad8
Vincent Gay <glurbi@gmail.com>
2018-04-02 13:56:42 +0200

    dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adventofcode2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
