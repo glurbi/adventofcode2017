b95c83a984c6762643a2eb3100175ac3a45a563f
Vincent Gay <glurbi@gmail.com>
2018-04-14 11:40:08 +0200

    day 5 part 2

--- Day5.cs
+++ Day5.cs
@@ -26,5 +26,29 @@ namespace adventofcode2017
 
             Console.WriteLine(count);
         }
+
+        internal static void Run2()
+        {
+            var input = File.ReadAllLines("input/Day5.txt");
+            //var input = new string[] { "0" , "3", "0", "1", "-3" };
+            var a = new int[input.Length];
+            
+            for (int i = 0; i < input.Length; i++)
+                a[i] = int.Parse(input[i]);
+
+            var pos = 0;
+            var count = 0;
+
+            while (pos >= 0 && pos < a.Length)
+            {
+                var inc = a[pos] < 3 ? 1 : -1;
+                a[pos] += inc;
+                pos = pos + a[pos] - inc;
+                count++;
+            }
+
+            Console.WriteLine(count);
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace adventofcode2017
     {
         static void Main(string[] args)
         {
-            Day5.Run1();
+            Day5.Run2();
         }
     }
 }
