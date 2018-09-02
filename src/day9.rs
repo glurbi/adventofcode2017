93a90a237f64604ade7061c4353ae66061b40d46
Vincent Gay <glurbi@gmail.com>
2018-09-02 11:24:40 +0200

    day 9 - 1

---
+++ src/day9.rs
@@ -0,0 +1,61 @@
+use std::fs::File;
+use std::io::prelude::*;
+use pest::Parser;
+use pest::iterators::Pairs;
+
+#[cfg(debug_assertions)]
+const _GRAMMAR: &'static str = include_str!("day9.pest");
+
+#[derive(Parser)]
+#[grammar = "day9.pest"]
+pub struct StreamParser;
+
+pub fn day9() {
+
+    let examples = vec![
+        "{}",
+        "{{{}}}",
+        "{{},{}}",
+        "{{{},{},{{}}}}",
+        "{<{},{},{{}}>}",
+        "{<a>,<a>,<a>,<a>}",
+        "{{<a>},{<a>},{<a>},{<a>}}",
+        "{{<!>},{<!>},{<!>},{<a>}}",
+        "{{<ab>},{<ab>},{<ab>},{<ab>}}",
+        "{{<!!>},{<!!>},{<!!>},{<!!>}}",
+        "{{<a!>},{<a!>},{<a!>},{<ab>}}"
+    ];
+
+    for ex in examples.iter() {
+        println!("{:?}", StreamParser::parse(Rule::stream, ex));
+    }
+
+    for ex in examples {
+        println!("----------------");
+        let mut score = 0;
+        let pairs = StreamParser::parse(Rule::stream, ex).unwrap_or_else(|e| panic!("{}", e));
+        visit(pairs, 1, &mut score);
+        println!("{} -> {}", ex, score);
+    }
+
+    let file_name = "input/day9.txt";
+    let err_open = format!("Failed to open {}", &file_name);
+    let mut file = File::open(file_name).expect(&err_open);
+    let mut stream = String::new();
+    let err_read = format!("Failed to read {}", &file_name);
+    file.read_to_string(&mut stream).expect(&err_read);
+
+    println!("----------------");
+    let mut score = 0;
+    let pairs = StreamParser::parse(Rule::stream, &stream).unwrap_or_else(|e| panic!("{}", e));
+    visit(pairs, 1, &mut score);
+    println!("{} -> {}", "...", score);
+
+}
+
+fn visit(pairs: Pairs<Rule>, depth: i32, score: &mut i32) {
+    for pair in pairs {
+        *score += depth;
+        visit(pair.into_inner(), depth + 1, score);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 extern crate regex;
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
 
 //mod day6;
 //mod day7;
-mod day8;
+//mod day8;
+mod day9;
 
 fn main() {
     //day6::day6();
     //::day7::day7();
-    day8::day8();
+    //day8::day8();
+    day9::day9();
 }
