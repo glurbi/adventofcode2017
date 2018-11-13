87dc996bf4285fac8490cf51c4f1e5c1a7bea222
Vincent Gay <glurbi@gmail.com>
2018-11-13 22:31:13 +0100

    day 15 - 1

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,8 @@
 //mod day11;
 //mod day12;
 //mod day13;
-mod day14;
+//mod day14;
+mod day15;
 
 fn main() {
     //day6::day6();
@@ -24,5 +25,6 @@ fn main() {
     //day11::day11();
     //day12::day12();
     //day13::day13();
-    day14::day14();
+    //day14::day14();
+    day15::day15();
 }
